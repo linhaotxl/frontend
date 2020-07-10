d679bc076f2bab84f5db977998ae0e560c2a8f3e
linhaotxl <602523587@qq.com>
2020-07-10 16:59:21 +0800

    移除 twm

--- packages/twm/test/utils/date.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function date() { }
-exports.default = date;
--- packages/twm/test/utils/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function test() { }
-exports.default = test;
